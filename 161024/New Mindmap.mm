6ff3e1facc4e7c6fa084f6824dcaf788b334de3f
Elhe <elhe@localhost.localdomain>
2016-10-25 09:11:05 +0200

    added a new mond map

---
+++ 161024/New Mindmap.mm
@@ -0,0 +1,215 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476713562310" ID="ID_36107862" MODIFIED="1476776920059" STYLE="bubble" TEXT="NN learning processes. Statistical nature">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714056647" HGAP="-220" ID="ID_758047931" MODIFIED="1476776945331" POSITION="right" STYLE="bubble" TEXT="Regressive model" VSHIFT="293">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714369040" ID="ID_607819820" MODIFIED="1476776920054" STYLE="bubble" TEXT="Bias/variance dilemma">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714408140" ID="ID_435761199" MODIFIED="1476776920052" STYLE="bubble" TEXT="Bias = approximation error (AE)">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714419800" ID="ID_1107954293" MODIFIED="1476776920050" STYLE="bubble" TEXT="Variance = manifestation of AE">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714534187" ID="ID_1650040457" MODIFIED="1476776920049" STYLE="bubble" TEXT="Properties">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714064934" ID="ID_76278034" MODIFIED="1476776920047" STYLE="bubble" TEXT="Zero mean value">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714107342" ID="ID_241346750" MODIFIED="1476776920045" STYLE="bubble" TEXT="Error is uncorrelated with regression function">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714526075" HGAP="-716" ID="ID_1831141673" MODIFIED="1476776933747" POSITION="left" STYLE="bubble" TEXT="Statistical learning theory" VSHIFT="-490">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714553959" ID="ID_842038733" MODIFIED="1476776920039" STYLE="bubble" TEXT="Supervised learning model">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714574215" ID="ID_32200486" MODIFIED="1476776920038" STYLE="bubble" TEXT="Environment">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714582403" ID="ID_1303881424" MODIFIED="1476776920036" STYLE="bubble" TEXT="Teacher">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714585679" ID="ID_1486312681" MODIFIED="1476776920035" STYLE="bubble" TEXT="Learning machine/algorithm">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714686832" ID="ID_654001494" MODIFIED="1476776920033" STYLE="bubble" TEXT="Important definitions">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714692502" ID="ID_1722623555" MODIFIED="1476776920032" STYLE="bubble" TEXT="Convergence in probability">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714702322" ID="ID_349400888" MODIFIED="1476776920031" STYLE="bubble" TEXT="Supremum and infimum">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714754695" ID="ID_1857721265" MODIFIED="1476776920029" STYLE="bubble" TEXT="Empirical risk functional">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714755764" ID="ID_1662851214" MODIFIED="1476776920028" STYLE="bubble" TEXT="Strict consistensy">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714792970" ID="ID_1117101401" MODIFIED="1476776920027" STYLE="bubble" TEXT="Principle of empirical risk minimization">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714824364" ID="ID_573148293" MODIFIED="1476776920025" STYLE="bubble" TEXT="Independent on unknown distribution function">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714871235" ID="ID_1530131992" MODIFIED="1476776920024" STYLE="bubble" TEXT="Can be minimize with respect to weight vector">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714953816" ID="ID_269085760" MODIFIED="1476776920022" STYLE="bubble" TEXT="Steps">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714957545" ID="ID_1933254755" MODIFIED="1476776920020" STYLE="bubble" TEXT="Conctruct empirical risk functional (RF)">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714977785" ID="ID_1984094639" MODIFIED="1476776920019" STYLE="bubble" TEXT="Choose a wight vector that minimizes RF">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476714972199" ID="ID_1653383136" MODIFIED="1476776920018" STYLE="bubble" TEXT="Difine a uniform convergence">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715060182" ID="ID_1500902679" MODIFIED="1476776920016" STYLE="bubble" TEXT="VC dimension">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715083715" ID="ID_683225613" MODIFIED="1476776920015" STYLE="bubble" TEXT="Theory of uniform convergence of RF">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715312238" ID="ID_1689617" MODIFIED="1476776920013" STYLE="bubble" TEXT="Dichotomy (D)- function for binary classification">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715095035" ID="ID_16838227" MODIFIED="1476776920010" STYLE="bubble" TEXT="Shattering - VC includes all possible D">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715495928" ID="ID_62603300" MODIFIED="1476776920008" STYLE="bubble" TEXT="Is purely combinatorial concept">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715518938" ID="ID_313193275" MODIFIED="1476776920006" STYLE="bubble" TEXT="Heaviside activation function">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715531544" ID="ID_1086034851" MODIFIED="1476776920005" STYLE="bubble" TEXT="Sigmoid activation function">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715450005" HGAP="64" ID="ID_550603850" MODIFIED="1476776920000" STYLE="bubble" TEXT="Constructive distribution-free bounds on the generalization ability of learning machines" VSHIFT="-86">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716366322" ID="ID_225263287" MODIFIED="1476776919998" STYLE="bubble" TEXT="Interpretations">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715577876" ID="ID_1258204019" MODIFIED="1476776919996" STYLE="bubble" TEXT="RF - probaility of classification error">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715594623" ID="ID_873602616" MODIFIED="1476776919995" STYLE="bubble" TEXT="RF - training error">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716376966" ID="ID_773133304" MODIFIED="1476776919993" STYLE="bubble" TEXT="Bounds">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716381235" ID="ID_1567320784" MODIFIED="1476776919991" STYLE="bubble" TEXT="On the rate of uniform convergence">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716397610" ID="ID_569239377" MODIFIED="1476776919988" STYLE="bubble" TEXT="For near zero error">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716414176" ID="ID_1554339724" MODIFIED="1476776919979" STYLE="bubble" TEXT="For large error">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715698949" ID="ID_67751081" MODIFIED="1476776919978" STYLE="bubble" TEXT="Structural risk minimization">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715707371" ID="ID_38229766" MODIFIED="1476776919975" STYLE="bubble" TEXT="Empirical risk minimization">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715768229" ID="ID_1799556118" MODIFIED="1476776919973" STYLE="bubble" TEXT="Pattern classifier with the smallest risk choice">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715842901" HGAP="-489" ID="ID_156856151" MODIFIED="1476776950090" POSITION="right" STYLE="bubble" TEXT="Probably approximately correct (PAC) model of learning" VSHIFT="38">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715940157" HGAP="14" ID="ID_121736955" MODIFIED="1476776919961" STYLE="bubble" TEXT="Target concept is treated as a function from environment set (concept)" VSHIFT="-17">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476715978294" ID="ID_1806844326" MODIFIED="1476776919959" STYLE="bubble" TEXT="Examples are statistically independent">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716045148" ID="ID_147355090" MODIFIED="1476776919957" STYLE="bubble" TEXT="Parameters">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716052632" ID="ID_1267378673" MODIFIED="1476776919955" STYLE="bubble" TEXT="Error parameter">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716059486" ID="ID_1888024599" MODIFIED="1476776919952" STYLE="bubble" TEXT="Confidence parameter">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716077248" ID="ID_1878723439" MODIFIED="1476776919950" STYLE="bubble" TEXT="Sample complexity">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716105694" ID="ID_1314620707" MODIFIED="1476776919949" STYLE="bubble" TEXT="Any consistent NN learning algorithm is PAC">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716125826" ID="ID_1678325723" MODIFIED="1476776919947" STYLE="bubble" TEXT="Sufficient size of training set is a constant">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cbeeec" CREATED="1476716158841" ID="ID_1391254595" MODIFIED="1476776919945" STYLE="bubble" TEXT="Computational complexity">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</node>
+</map>
