318eae35df4e0d27c818521aaf0136b4eb25d9c8
Elhe <elhe@localhost.localdomain>
2016-11-02 21:48:18 +0100

    added empty files for a new assignment

---
+++ 161031/Single-Layer Perceptrons.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1478096247122" ID="ID_1781678699" MODIFIED="1478096265800" TEXT="Single-Layer Perceptrons">
+<node CREATED="1478096471421" ID="ID_579627297" MODIFIED="1478096471421" POSITION="right" TEXT=""/>
+</node>
+</map>
