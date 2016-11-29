c2289b97e804bcb94e666d1c497c28cef852aaa0
Elhe <elhe@localhost.localdomain>
2016-11-29 18:06:57 +0100

    added questions, mindmap and tasks text

---
+++ 161128/SOMs.mm
@@ -0,0 +1,259 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436167406" ID="ID_522816404" MODIFIED="1480438432110" STYLE="bubble" TEXT="SOMs">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436287079" HGAP="28" ID="ID_1766223329" MODIFIED="1480438457178" POSITION="left" STYLE="bubble" TEXT="Introduction" VSHIFT="-64">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436274540" ID="ID_432111405" MODIFIED="1480438432109" STYLE="bubble" TEXT="Based on competetive learning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436302004" ID="ID_479507577" MODIFIED="1480438432108" STYLE="bubble" TEXT="Mostly one or two dimensional">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436341162" ID="ID_1729968867" MODIFIED="1480438432104" STYLE="bubble" TEXT="Levels of adaptation">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436362712" ID="ID_1329488034" MODIFIED="1480438432103" STYLE="bubble" TEXT="Microscopic of single neuron">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436382444" ID="ID_483329440" MODIFIED="1480438432102" STYLE="bubble" TEXT="Microscopic of neuron layers">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436413580" ID="ID_1757777857" MODIFIED="1480438432101" STYLE="bubble" TEXT="SOM - nonlinear generalization of PCA">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436430878" HGAP="-97" ID="ID_1323607278" MODIFIED="1480438442966" POSITION="right" STYLE="bubble" TEXT="Two basic feature-mapping models" VSHIFT="-144">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436444269" ID="ID_1946941365" MODIFIED="1480438432099" STYLE="bubble" TEXT="Properties">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436492838" ID="ID_908336011" MODIFIED="1480438432096" STYLE="bubble" TEXT="Each piece of information is kept &#xa;in a proper context ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436513689" ID="ID_441335533" MODIFIED="1480438432091" STYLE="bubble" TEXT="Neurons are closed together, so &#xa;they interact via short synaptic &#xa;connections&#xa;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436580461" ID="ID_588199171" MODIFIED="1480438432090" STYLE="bubble" TEXT="Principle of topographic map formation">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436683979" ID="ID_861400611" MODIFIED="1480438432083" STYLE="bubble" TEXT="The spatial location of an output neuron &#xa;in a topographic map corresponds to a&#xa; particular domain of data drawn from&#xa; the input space">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436699439" ID="ID_1533269628" MODIFIED="1480438432082" STYLE="bubble" TEXT="The models">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436717635" ID="ID_876468855" MODIFIED="1480438432082" STYLE="bubble" TEXT="Willshaw-von der Malsburg&apos;s model">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436793715" ID="ID_1059004749" MODIFIED="1480438432081" STYLE="bubble" TEXT="Hebbian type">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436814779" ID="ID_1051068760" MODIFIED="1480438432080" STYLE="bubble" TEXT="Input dim = output dim">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436743815" ID="ID_149109986" MODIFIED="1480438432080" STYLE="bubble" TEXT="Kohonen model">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436840850" ID="ID_1404614057" MODIFIED="1480438432079" STYLE="bubble" TEXT="More general">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436843709" ID="ID_1693444108" MODIFIED="1480438432078" STYLE="bubble" TEXT="Vector-coding algorithms">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436873974" HGAP="16" ID="ID_1224104885" MODIFIED="1480438449899" POSITION="right" STYLE="bubble" TEXT="SOM" VSHIFT="2">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436878440" HGAP="-48" ID="ID_1771130278" MODIFIED="1480438445507" STYLE="bubble" TEXT="Goal" VSHIFT="-86">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480436884627" ID="ID_926067823" MODIFIED="1480438432074" STYLE="bubble" TEXT="To transform an incoming signal pattern of arbitrary&#xa; dimension into one- or two-dimensional discrete &#xa;map in a topologically ordered fashion">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437069088" ID="ID_679723622" MODIFIED="1480438432073" STYLE="bubble" TEXT="Formation processes">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437119916" ID="ID_18077110" MODIFIED="1480438432072" STYLE="bubble" TEXT="Competition">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437245989" ID="ID_1182531294" MODIFIED="1480438432072" STYLE="bubble" TEXT="Condition i(x) = argmin_j||x-w_j|| , j = 1,2,...,l">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437307989" ID="ID_1935323141" MODIFIED="1480438432071" STYLE="bubble" TEXT="Best-matching neuron">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437123277" ID="ID_1413687247" MODIFIED="1480438432070" STYLE="bubble" TEXT="Cooperation">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437332524" ID="ID_101408870" MODIFIED="1480438432070" STYLE="bubble" TEXT="Winning neuron neighborhood">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437386588" ID="ID_431523296" MODIFIED="1480438432069" STYLE="bubble" TEXT="All neurons in a certain radius">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437427013" ID="ID_444679743" MODIFIED="1480438432068" STYLE="bubble" TEXT="Exp decay with time">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437127679" ID="ID_747161353" MODIFIED="1480438432067" STYLE="bubble" TEXT="Synaptic adaptation">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437470970" ID="ID_1906914870" MODIFIED="1480438432066" STYLE="bubble" TEXT="Include forgetting term into Hebbian theory">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437502765" ID="ID_624135163" MODIFIED="1480438432059" STYLE="bubble" TEXT="&#x394;w_j = &#x3b7;y_jx-g(y_j)w_j">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437570513" ID="ID_31743847" MODIFIED="1480438432058" STYLE="bubble" TEXT="Phases">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437572779" ID="ID_691953649" MODIFIED="1480438432056" STYLE="bubble" TEXT="Ordering">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437594585" ID="ID_1212176048" MODIFIED="1480438432055" STYLE="bubble" TEXT="&gt;1000 iterations">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437613700" ID="ID_1388248534" MODIFIED="1480438432054" STYLE="bubble" TEXT="Decrease of learning rate">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437622836" ID="ID_1072480064" MODIFIED="1480438432050" STYLE="bubble" TEXT="Neighborhood at one moment includes &#xa;all neurons and then shrinks ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437575284" ID="ID_226064796" MODIFIED="1480438432049" STYLE="bubble" TEXT="Convergence">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437705445" ID="ID_1987884102" MODIFIED="1480438432048" STYLE="bubble" TEXT="Maintain small &#x3b7;">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437711144" ID="ID_98302085" MODIFIED="1480438432047" STYLE="bubble" TEXT="Only nearest neighbors or even only one neuron">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437893680" ID="ID_1708609253" MODIFIED="1480438432046" STYLE="bubble" TEXT="Algorithm">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437899388" ID="ID_1818901676" MODIFIED="1480438432045" STYLE="bubble" TEXT="Initialization">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437903662" ID="ID_855346568" MODIFIED="1480438432043" STYLE="bubble" TEXT="Samplimg">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437907192" ID="ID_1591127476" MODIFIED="1480438432042" STYLE="bubble" TEXT="Similarity matching">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437914143" ID="ID_1563854976" MODIFIED="1480438432040" STYLE="bubble" TEXT="Updating">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437918394" ID="ID_1977635051" MODIFIED="1480438432038" STYLE="bubble" TEXT="Continuation">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437876344" ID="ID_29444598" MODIFIED="1480438432037" POSITION="left" STYLE="bubble" TEXT="Feature map properties">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437883948" ID="ID_1814952066" MODIFIED="1480438432035" STYLE="bubble" TEXT="Continious input &amp; discrete output">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480437974179" ID="ID_837149978" MODIFIED="1480438432034" STYLE="bubble" TEXT="Approximation of the input space">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438000476" ID="ID_435343100" MODIFIED="1480438432032" STYLE="bubble" TEXT="Topological ordering">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438005146" ID="ID_1350049615" MODIFIED="1480438432031" STYLE="bubble" TEXT="Density matching">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438015614" ID="ID_1028010592" MODIFIED="1480438432030" STYLE="bubble" TEXT="Feature selection">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438086510" HGAP="-8" ID="ID_1185780490" MODIFIED="1480438454525" POSITION="right" STYLE="bubble" TEXT="Vector quantization" VSHIFT="30">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438164032" ID="ID_1764342169" MODIFIED="1480438432028" STYLE="bubble" TEXT="Hierarchical VQ">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438210257" ID="ID_97158666" MODIFIED="1480438432027" STYLE="bubble" TEXT="Learning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438116296" ID="ID_1039182687" MODIFIED="1480438432026" STYLE="bubble" TEXT="Voronoi">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438131012" ID="ID_1428790377" MODIFIED="1480438432025" STYLE="bubble" TEXT="Supervised learning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438256491" HGAP="12" ID="ID_39694667" MODIFIED="1480438461168" POSITION="left" STYLE="bubble" TEXT="Contextual maps" VSHIFT="52">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438265614" ID="ID_649726397" MODIFIED="1480438419996" STYLE="bubble" TEXT="Views">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438283177" ID="ID_101627566" MODIFIED="1480438432018" STYLE="bubble" TEXT="Elastic net">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node BACKGROUND_COLOR="#cdf4f1" CREATED="1480438286505" ID="ID_1927877534" MODIFIED="1480438432016" STYLE="bubble" TEXT="Class labels">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
